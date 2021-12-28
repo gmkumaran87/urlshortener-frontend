e76f122ac9a35f4cca46bb3d532841e1d9c35e33
Muthukumaran <gmkumaran87@gmail.com>
2021-12-28 11:52:59 +0530

    Navbar and main page components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Link, NavLink } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Main;
